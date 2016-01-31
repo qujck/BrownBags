3fe7e9b6af7ca33b9cad58d93245271a51de61a8
Peter Parker <social@qujck.com>
2016-01-31 13:52:51 +0000

    Initial commit

---
+++ Bag3/IntegrationTests/FileReader.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IntegrationTests
+{
+    /* Bag 3:
+       Add Csv delimited, Tab delimited and Xml delimited
+       Then redo as unit testable
+       Talk about the benefits of tests in general, refactor the new code into Linq statements
+
+       Validation:
+        
+           Name:
+               cannot contain numbers
+               cannot be null
+               > 1 character
+               < 256 characters
+
+           Dept:
+               can be null
+               > 1 character
+               < 256 characters
+
+           Employee Number:
+               cannot be null
+               must be 3 characters + 8 numbers
+    */
+
+    // Bag 4:
+    // Show a generic query abstraction
+    // refactor to parameter objects
+    // add logging with a static class
+    // And then we have AOP :-)
+    public sealed class FileReader
+    {
+        public List<FileData> Read(string filename, FileType fileType)
+        {
+            var result = new List<FileData>();
+            switch (fileType)
+            {
+                case FileType.Csv:
+                    string[] lines = File.ReadAllLines(filename);
+                    foreach (string line in lines)
+                    {
+                        string[] split = line.Split(',');
+                        var dataItem = new FileData();
+                        dataItem.Fieldname = split[0];
+                        dataItem.StringValue = split[1];
+                        result.Add(dataItem);
+                    }
+
+                    return result;
+                case FileType.Tab:
+                    string[] lines2 = File.ReadAllLines(filename);
+                    foreach (string line in lines2)
+                    {
+                        string[] split = line.Split('\t');
+                        var dataItem = new FileData();
+                        dataItem.Fieldname = split[0];
+                        dataItem.StringValue = split[1];
+                        result.Add(dataItem);
+                    }
+
+                    return result;
+                case FileType.Xml:
+                    var doc = XDocument.Load(filename);
+                    foreach(var child in doc.Root.Elements())
+                    {
+                        var dataItem = new FileData();
+                        dataItem.Fieldname = child.Name.LocalName;
+                        dataItem.StringValue = child.Value;
+                        result.Add(dataItem);
+                    }
+
+                    return result;
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+
+    public enum FileType
+    {
+        Xml,
+        Csv,
+        Tab
+    }
+
+    public class FileData
+    {
+        public string Fieldname { get; set; }
+
+        public string StringValue { get; set; }
+    }
+}
---
+++ Bag3/IntegrationTests/FileReaderTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IntegrationTests
+{
+    public class FileReaderTests
+    {
+        [TestCase(0, "Name", "Peter")]
+        [TestCase(1, "Dept", "DMS")]
+        [TestCase(2, "EmployeeNumber", "12345")]
+        public void FileReader_GivenCsvFile_ReturnsFileDataForEachLine(
+            int index, string field, string value)
+        {
+            var fileReader = new FileReader();
+            var data = fileReader.Read(@"D:\Peter\OneDrive\DEV\BrownBags\Bag3\IntegrationTests\data.csv", FileType.Csv);
+            Assert.AreEqual(3, data.Count);
+            Assert.AreEqual(field, data[index].Fieldname);
+            Assert.AreEqual(value, data[index].StringValue);
+        }
+
+        [TestCase(0, "Name", "Peter")]
+        [TestCase(1, "Dept", "DMS")]
+        [TestCase(2, "EmployeeNumber", "12345")]
+        public void FileReader_GivenTabFile_ReturnsFileDataForEachLine(
+            int index, string field, string value)
+        {
+            var fileReader = new FileReader();
+            var data = fileReader.Read(@"D:\Peter\OneDrive\DEV\BrownBags\Bag3\IntegrationTests\data.tab", FileType.Tab);
+            Assert.AreEqual(3, data.Count);
+            Assert.AreEqual(field, data[index].Fieldname);
+            Assert.AreEqual(value, data[index].StringValue);
+        }
+
+        [TestCase(0, "Name", "Peter")]
+        [TestCase(1, "Dept", "DMS")]
+        [TestCase(2, "EmployeeNumber", "12345")]
+        public void FileReader_GivenXmlFile_ReturnsFileDataForEachLine(
+            int index, string field, string value)
+        {
+            var fileReader = new FileReader();
+            var data = fileReader.Read(@"D:\Peter\OneDrive\DEV\BrownBags\Bag3\IntegrationTests\data.xml", FileType.Xml);
+            Assert.AreEqual(3, data.Count);
+            Assert.AreEqual(field, data[index].Fieldname);
+            Assert.AreEqual(value, data[index].StringValue);
+        }
+    }
+}
