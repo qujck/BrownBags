83c62a2563d88026520c7d428589fa445d3ac8b7
Peter Parker <social@qujck.com>
2016-02-20 23:54:09 +0000

    Added composition root and configured as console apps

---
+++ Bag4/Solid.Tests/ProgramTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Solid.Tests.Unit
+{
+    public class ProgramTests
+    {
+        public ProgramTests()
+        {
+            var standardOut = new StreamWriter(Console.OpenStandardOutput());
+            standardOut.AutoFlush = true;
+            Console.SetOut(standardOut);
+        }
+
+        [TestCase("data.csv")]
+        [TestCase("data.tab")]
+        [TestCase("data.xml")]
+        public void Program_WithDataFile_Succeeds(string filename)
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                Program.Main(new[] { filename });
+
+                Assert.AreEqual("Completed\r\n", sw.ToString());
+            }
+        }
+    }
+}
---
+++ Bag4/Solid/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solid.FileReaders;
+using Solid.Model;
+using Solid.Validators.CrossField;
+using Solid.Validators.SingleField;
+
+namespace Solid
+{
+    public class Program
+    {
+        static readonly IFileReader[] readers;
+        static readonly ISingleFieldValidator doesNotContainANumber;
+        static readonly ISingleFieldValidator isNotEmpty;
+        static readonly ISingleFieldValidator isNotLongerThan256;
+        static readonly ISingleFieldValidator isNotNull;
+        static readonly ISingleFieldValidator isValidEmployeeNumber;
+        static readonly ICrossFieldValidator validateAppsTitle;
+        static readonly ICrossFieldValidator validateArchitectTitle;
+        static readonly ICrossFieldValidator validateBoardEmployeeNumber;
+        static readonly ICrossFieldValidator validateFinanceEmployeeNumber;
+        static readonly ICrossFieldValidator validateNonArchitectAndNonAppsTitle;
+        static readonly ICrossFieldValidator validateNonFinanceNonBoardEmployeeNumber;
+
+        public static void Main(string[] args)
+        {
+            IEnumerable<FileData> data = readers.Single(reader => reader.CanRead(args[0])).Read(args[0]);
+
+            doesNotContainANumber.Validate(data.Name());
+            isNotNull.Validate(data.Name());
+            isNotEmpty.Validate(data.Name());
+            isNotLongerThan256.Validate(data.Name());
+
+            isNotEmpty.Validate(data.Department());
+            isNotLongerThan256.Validate(data.Department());
+
+            isNotNull.Validate(data.EmployeeNumber());
+            isValidEmployeeNumber.Validate(data.EmployeeNumber());
+
+            validateAppsTitle.Validate(data);
+            validateArchitectTitle.Validate(data);
+            validateBoardEmployeeNumber.Validate(data);
+            validateFinanceEmployeeNumber.Validate(data);
+            validateNonArchitectAndNonAppsTitle.Validate(data);
+            validateNonFinanceNonBoardEmployeeNumber.Validate(data);
+
+            Console.WriteLine("Completed");
+        }
+
+        static Program()
+        {
+            readers = new IFileReader[]
+            {
+                new CsvReader(),
+                new TabularReader(),
+                new XmlReader()
+            };
+            doesNotContainANumber = new DoesNotContainANumber();
+            isNotEmpty = new IsNotEmpty();
+            isNotLongerThan256 = new IsNotLongerThan256();
+            isNotNull = new IsNotNull();
+            isValidEmployeeNumber = new IsValidEmployeeNumber();
+            validateAppsTitle = new ValidateAppsTitle();
+            validateArchitectTitle = new ValidateArchitectTitle();
+            validateBoardEmployeeNumber = new ValidateBoardEmployeeNumber();
+            validateFinanceEmployeeNumber = new ValidateFinanceEmployeeNumber();
+            validateNonArchitectAndNonAppsTitle = new ValidateNonArchitectAndNonAppsTitle();
+            validateNonFinanceNonBoardEmployeeNumber = new ValidateNonFinanceNonBoardEmployeeNumber();
+        }
+    }
+}
