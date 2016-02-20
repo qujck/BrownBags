fa799be98a1cc2dfeba51536444bc7720ce848b0
Peter Parker <social@qujck.com>
2016-02-20 20:44:18 +0000

    Solid reader tests

---
+++ Bag4/Solid.Tests/FileReaders/XmlReaderTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Solid.FileReaders;
+using Solid.Model;
+
+namespace Solid.Tests.Unit.FileReaders
+{
+    public class XmlReaderTests
+    {
+        [Test]
+        public void CanTest_ForFileNamesWithXmlExtension_IsTrue()
+        {
+            var reader = new XmlReader();
+
+            Assert.True(reader.CanRead("file.xml"));
+        }
+
+        [TestCase("file.xm")]
+        [TestCase("file.xml1")]
+        [TestCase("xml.abc")]
+        public void CanTest_ForFileNamesNotWithXmlExtension_IsFalse(string filename)
+        {
+            var reader = new XmlReader();
+
+            Assert.False(reader.CanRead(filename));
+        }
+
+        [Test]
+        public void Reader_Always_ReadsData()
+        {
+            var reader = new XmlReader();
+
+            var result = reader.Read("data.xml");
+
+            Assert.AreEqual("Paul", result.Name().StringValue);
+            Assert.AreEqual("Board", result.Department().StringValue);
+            Assert.AreEqual("BRD12345678", result.EmployeeNumber().StringValue);
+            Assert.AreEqual("CTO", result.Title().StringValue);
+        }
+    }
+}
---
+++ Bag4/Solid/Model/FileDataExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.Model
+{
+    public static class FileDataExtensions
+    {
+        public static FileData Department(this IEnumerable<FileData> data) =>
+            data.Single(item => item.Fieldname == "Dept");
+
+        public static FileData Name(this IEnumerable<FileData> data) =>
+            data.Single(item => item.Fieldname == "Name");
+
+        public static FileData EmployeeNumber(this IEnumerable<FileData> data) =>
+            data.Single(item => item.Fieldname == "EmployeeNumber");
+
+        public static FileData Title(this IEnumerable<FileData> data) =>
+            data.Single(item => item.Fieldname == "Title");
+    }
+}
