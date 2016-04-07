4e452281fbb89e81394b79bbdbbbe5ca9c75b116
qujck <peter@qujck.com>
2016-04-07 20:15:54 +0100

    CQRS getting started

---
+++ CQRS/CQRS/FibonnaciQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS
+{
+    public class FibonnaciQuery : IQuery<IEnumerable<double>>
+    {
+        public double Min { get; set; }
+        public double Max { get; set; }
+    }
+}
